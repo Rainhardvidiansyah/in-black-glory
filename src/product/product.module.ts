581d04e1afd9bbd16f395c41b34eb468f681a59e
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-07-11 11:01:23 +0700

    added two new providers in ProductModule productvariant service and product varian providers ✅✅✅

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -4,13 +4,17 @@ import { ProductController } from './product.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { productProviders } from './product.providers';
 import { RedisConfigModule } from 'src/redisconfig/redis-config.module';
+import { ProductVariantService } from './product-variant.service';
+import { productVariantProviders } from './product-variant.providers';
 
 
 @Module({
 
   imports: [DatabaseModule, RedisConfigModule],
  
-  providers: [ProductService, ...productProviders],
+  providers: [
+    ProductService, ...productProviders, 
+    ProductVariantService, ...productVariantProviders],
 
   controllers: [ProductController]
 })
