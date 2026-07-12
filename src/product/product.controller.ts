81d30374d93773fe61359c5e64878e2b55256a54
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-07-12 11:55:33 +0700

    Added product variants functionalities to product controller class ✅✅✅

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -5,13 +5,21 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { CreateProductDto } from './dto/create-product.dto';
 import { Roles } from 'src/common/decorators/roles.decorators';
 import { Role } from 'src/common/enums/role.enum';
+import { ProductVariantService } from './product-variant.service';
+import { CreateProductVariantDto } from './dto/create-product-variant.dto';
+import { Public } from 'src/common/decorators/public.decorators';
+import { ProductVariantResponse } from './dto/product-varian.response.dto';
+import { ProductResponseDto } from './dto/product-response.dto';
 
 @Controller('products')
 export class ProductController {
 
   private logger = new Logger(ProductController.name);
 
-  constructor(private readonly productsService: ProductService) {}
+  constructor(
+    private readonly productsService: ProductService,
+    private readonly variantService: ProductVariantService,
+  ) {}
 
 
   @ResponseMessage('Product created successfully')
@@ -32,7 +40,7 @@ export class ProductController {
   }
 
 
-  
+  @Public()
   @ResponseMessage('All products fetched successfully')
   @Get()
   async getAllProducts(){
@@ -55,6 +63,40 @@ export class ProductController {
   async deleteProductById(id: string){
     this.productsService.deleteProductById(id);
   }
+
+
+  // ======== PRODUCT VARIANT ======== //
+
+  @ResponseMessage('Id product variant has been fetched successfully')
+  @Get("variants/:variantId")
+  async getVarianId(@Param("variantId", new ParseUUIDPipe({version: '7'})) variantId: string){
+    return this.variantService.getVariantById(variantId);
+  }
+
+
+  //Get Varian by product id
+  @ResponseMessage('Product variants have been fetched successfully')
+  @Get(":productId/variants")
+  async findVariantByProductId(@Param("productId", new ParseUUIDPipe({version: '7'})) productId: string){
+    this.logger.log(`findVariantByProductId is hit`);
+
+    return await this.variantService.getVariantByProductId(productId);
+
+  }
+
+  //Create Product Variant
+  @ResponseMessage('Variant created successfully')
+  @Post(':productId/variant')
+  @Roles(Role.ADMIN)
+  async createProductVarian(@Param("productId", new ParseUUIDPipe({version: '7'})) productId: string, @Body() variantDto: CreateProductVariantDto){
+    const varian = await this.variantService.createProductVarian(productId, variantDto);
+    return new ProductVariantResponse(varian);
+  }
+
+  
+
+
+
 }
 
 
