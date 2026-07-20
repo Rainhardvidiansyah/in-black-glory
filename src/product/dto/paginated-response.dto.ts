30b588f652aecf30c8c04c11085e882be18c6e39
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-07-21 06:42:19 +0700

    create two dto files to paginate data product paginated response and pagination query ✅✅✅

---
+++ src/product/dto/paginated-response.dto.ts
@@ -0,0 +1,23 @@
+
+export class PaginatedResponse<T> {
+
+  readonly isPaginated = true;
+
+  data: T[];
+  meta: {
+    total: number;
+    page: number;
+    limit: number;
+    totalPages: number;
+  };
+
+  constructor(data: T[], total: number, page: number, limit: number) {
+    this.data = data;
+    this.meta = {
+      total,
+      page,
+      limit,
+      totalPages: Math.ceil(total / limit),
+    };
+  }
+}
